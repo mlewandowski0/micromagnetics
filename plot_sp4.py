08e2f49331b025ac693687b941530fba3633474c
Maciej Lewandowski <lewandowski.maciej1702@gmail.com>
2023-02-01 19:14:28 +0000

    code works from publication https://arxiv.org/abs/1411.7188

---
+++ plot_sp4.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+with open("sp4.dat") as f:
+    Mx, My, Mz, t = [], [], [], []
+    for line in f:
+        v = line.strip().split(" ")
+        t.append(float(v[0]))
+        Mx.append(float(v[1]))
+        My.append(float(v[2]))
+        Mz.append(float(v[3]))
+
+plt.plot(t, My, label="My", color="green")
+plt.legend()
+plt.savefig("My.png")
+plt.show()
+
+plt.plot(t, Mx, label="Mx", color="red")
+plt.plot(t, My, label="My", color="green")
+plt.plot(t, Mz, label="Mz", color="blue")
+plt.legend()
+plt.show()
